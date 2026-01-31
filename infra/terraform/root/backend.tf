39d12e0009e4be88c06bdabfe74194672f2459ed
조 성규 <sgjo@dimoa.co.kr>
2026-01-31 09:22:49 +0900

    add

--- infra/terraform/root/backend.tf
+++ infra/terraform/root/backend.tf
@@ -1,7 +1,7 @@
 terraform {
   backend "azurerm" {
     resource_group_name  = "sgjo-test-backend-rg"
-    storage_account_name = "testsgjonhplk7"
+    storage_account_name = "testsgjosm0qql"
     container_name       = "tfstate"
     key                  = "infra.tfstate"
   }
