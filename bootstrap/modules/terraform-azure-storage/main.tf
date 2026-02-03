361f7fb5496b9dd08a9ebfa2df77ec03b7201cc4
sgjo <zzz8052@naver.com>
2026-02-03 10:12:25 +0900

    edit

--- bootstrap/modules/terraform-azure-storage/main.tf
+++ bootstrap/modules/terraform-azure-storage/main.tf
@@ -1,11 +1,3 @@
-resource "random_string" "sa_suffix" {
-  length  = 6
-  upper   = false
-  lower   = true
-  numeric = true
-  special = false
-}
-
 resource "azurerm_storage_account" "storage" {
   name                     = var.storage_name
   resource_group_name      = var.rg_name
